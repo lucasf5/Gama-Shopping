13eb3e8ac5fb897ff3401a12bc369f205b898762
luccasecco <luccasecco@hotmail.com>
2022-09-07 12:36:15 -0300

    PI-006 + configuracoes

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,21 @@
 import { BrowserRouter } from "react-router-dom";
+import { ToastContainer } from "react-toastify";
 import { ThemeProvider } from "styled-components";
+import { CartProvider } from "./contexts/useCart";
 import { Router } from "./Router";
 import { GlobalStyle } from "./styles/global";
 import { defaultTheme } from "./styles/themes/default";
+
+import 'react-toastify/dist/ReactToastify.css';
 
 export function App() {
   return (
     <ThemeProvider theme={defaultTheme}>
       <BrowserRouter>
-        <Router />
+        <CartProvider>
+          <Router />
+          <ToastContainer autoClose={3000} />
+        </CartProvider>
         <GlobalStyle />
       </BrowserRouter>
     </ThemeProvider>

b4298b70d5b500abf253264763e8f80c0ff48c0a
Victor Amaral <victorab.dev@hotmail.com>
2022-09-08 19:34:20 -0300

    Criação tela login

--- src/pages/Login/index.tsx
+++ src/pages/Login/index.tsx
@@ -1,7 +1,40 @@
-import { Container } from "./styles";
+import { useState } from "react";
+import { Link } from "react-router-dom";
+import { Container, Inputs } from "./styles";
+import logo from "../../assets/logo.svg";
 
 export function Login() {
+  const [user, setUser] = useState();
+  const [password, setPassword] = useState();
+
+  function handleLogin() {
+    setUser(user);
+    setPassword(password);
+    console.log(`User: ${user}`, `Password: ${password}`);
+  }
   return (
-    <Container>Login</Container>
-  )
+    <Container>
+      <div>
+        <img src={logo} alt="Logo Gama Shopping em roxo" />
+      </div>
+
+      <Inputs>
+        <span>Login</span>
+        <input
+          type="text"
+          value={user}
+          onChange={(e) => setUser(e.target.value)}
+        />
+        <input
+          type="password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+        />
+        <p>
+          Não tem cadastro?<Link to="/novo_usuario">Criar agora</Link>
+        </p>
+        <button onClick={handleLogin}>Entrar</button>
+      </Inputs>
+    </Container>
+  );
 }
--- src/pages/Login/styles.ts
+++ src/pages/Login/styles.ts
@@ -1,3 +1,47 @@
 import styled from "styled-components";
 
-export const Container = styled.div``
+export const Container = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  margin-top: 200px;
+  margin-right: 100px;
+
+  img {
+    height: 515px;
+    width: 400px;
+  }
+
+  span {
+    font-size: 3rem;
+  }
+`;
+
+export const Inputs = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  margin-left: 200px;
+  gap: 20px;
+
+  input {
+    width: 410px;
+    height: 40px;
+    border: none;
+    background-color: #d9d9d9;
+    font-size: 1.5rem;
+  }
+
+  button {
+    width: 200px;
+    height: 60px;
+    background-color: #d9d9d9;
+    border: none;
+    font-size: 1.5rem;
+  }
+  a {
+    margin-left: 5px;
+    text-decoration: none;
+  }
+`;

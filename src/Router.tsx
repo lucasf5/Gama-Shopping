81ce5ccb60ed23f3fb053e374e74f4dd6cdf1cfb
Lucas Ferreira <lucasfpnt@gmail.com>
2022-09-21 12:01:36 -0300

    correcao router

--- src/Router.tsx
+++ src/Router.tsx
@@ -1,24 +1,24 @@
-import { Routes, Route } from 'react-router-dom'
-import Produto from './components/PI016-Produto'
-import { DefaultLayout } from './layouts/DefaultLayout'
-import { CadastroProduto } from './pages/CadastroProduto'
-import { CadastroUsuario } from './pages/CadastroUsuario'
-import { Checkout } from './pages/Checkout'
-import { Login } from './pages/Login'
-import { PaginaCliente } from './pages/PaginaCliente'
-import { Sucesso } from './pages/Sucesso'
+import { Routes, Route } from "react-router-dom";
+import Produto from "./components/PI016-Produto";
+import { DefaultLayout } from "./layouts/DefaultLayout";
+import { CadastroProduto } from "./pages/CadastroProduto";
+import { CadastroUsuario } from "./pages/CadastroUsuario";
+import { Checkout } from "./pages/Checkout";
+import { Login } from "./pages/Login";
+import { PaginaCliente } from "./pages/PaginaCliente";
+import { Sucesso } from "./pages/Sucesso";
 
 export function Router() {
   return (
     <Routes>
       <Route path="/" element={<Login />} />
+      <Route path="/novo_usuario" element={<CadastroUsuario />} />
       <Route path="/" element={<DefaultLayout />}>
-        <Route path="/novo_usuario" element={<CadastroUsuario />} />
         <Route path="/cliente" element={<PaginaCliente />} />
         <Route path="/carrinho" element={<Checkout />} />
         <Route path="/sucesso" element={<Sucesso />} />
         <Route path="/produto/:id" element={<Produto />} />
       </Route>
     </Routes>
-  )
+  );
 }
